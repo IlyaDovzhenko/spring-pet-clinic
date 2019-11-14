f1a5d7b765bae231f48283a9332bf881279a06f2
ilya <i.dovzhenko26@gmail.com>
2019-11-14 21:13:38 +0700

    Added in prev commit. Closes #8

--- pet-clinic-web/src/main/java/com/springframework/springpetclinic/controllers/IndexController.java
+++ pet-clinic-web/src/main/java/com/springframework/springpetclinic/controllers/IndexController.java
@@ -1,14 +1,13 @@
 package com.springframework.springpetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
 
     @GetMapping({"", "/", "index", "index.html"})
-    public String index(Model model) {
+    public String index() {
         return "index";
     }
 
