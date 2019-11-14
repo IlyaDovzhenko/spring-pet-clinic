481d38f7c3f5281a92d238506bf8d17f2cb1efdc
ilya <i.dovzhenko26@gmail.com>
2019-11-14 21:56:40 +0700

    Ref owners and vets maping

--- pet-clinic-web/src/main/java/com/springframework/springpetclinic/controllers/vets/OwnerController.java
+++ pet-clinic-web/src/main/java/com/springframework/springpetclinic/controllers/vets/OwnerController.java
@@ -2,12 +2,13 @@ package com.springframework.springpetclinic.controllers.vets;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequestMapping("/owners")
 @Controller
 public class OwnerController {
 
-    @GetMapping("/owners")
+    @RequestMapping({"","/"})
     public String listOwners(Model model) {
         return "/owners/owners_list";
     }
--- pet-clinic-web/src/main/java/com/springframework/springpetclinic/controllers/vets/VetController.java
+++ pet-clinic-web/src/main/java/com/springframework/springpetclinic/controllers/vets/VetController.java
@@ -3,11 +3,13 @@ package com.springframework.springpetclinic.controllers.vets;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequestMapping("/vets")
 @Controller
 public class VetController {
 
-    @GetMapping("/vets")
+    @GetMapping({"","/vets"})
     public String listVets(Model model) {
         return "vets/vets_list";
     }
