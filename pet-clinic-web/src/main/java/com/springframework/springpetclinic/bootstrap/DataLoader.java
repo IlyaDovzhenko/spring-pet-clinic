8492767a6c294b92f453a77c451c8451811937d0
ilya <i.dovzhenko26@gmail.com>
2019-11-20 09:36:37 +0700

    Refactored Map impl to abstract ID generator. Closes #24

--- pet-clinic-web/src/main/java/com/springframework/springpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/springframework/springpetclinic/bootstrap/DataLoader.java
@@ -4,8 +4,6 @@ import com.springframework.springpetclinic.model.Owner;
 import com.springframework.springpetclinic.model.Vet;
 import com.springframework.springpetclinic.services.OwnerService;
 import com.springframework.springpetclinic.services.VetService;
-import com.springframework.springpetclinic.services.map.OwnerServiceMap;
-import com.springframework.springpetclinic.services.map.VetServiceMap;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -23,14 +21,12 @@ public class DataLoader implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         Owner owner1 = new Owner();
-        owner1.setId(1L);
         owner1.setFirstName("Michael");
         owner1.setLastName("Weston");
 
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-        owner2.setId(2L);
         owner2.setFirstName("Fiona");
         owner2.setLastName("Glendale");
 
@@ -39,14 +35,12 @@ public class DataLoader implements CommandLineRunner {
         System.out.println("Loading owners by " + ownerService.getClass().getName());
 
         Vet vet1 = new Vet();
-        vet1.setId(1L);
         vet1.setFirstName("Sam");
         vet1.setLastName("Fisher");
 
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet2.setId(2L);
         vet2.setFirstName("Alex");
         vet2.setLastName("Long");
 
