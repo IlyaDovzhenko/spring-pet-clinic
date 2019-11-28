fa526fe6963051f71fb8ca15e3f060c192bd2a7c
ilya <i.dovzhenko26@gmail.com>
2019-11-28 11:21:30 +0700

    Added owner's contact info properties. Closes #29

--- pet-clinic-data/src/main/java/com/springframework/springpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/com/springframework/springpetclinic/model/Owner.java
@@ -8,5 +8,8 @@ import java.util.Set;
 @Getter
 @Setter
 public class Owner extends Person {
+    private String address;
+    private String city;
+    private String telephone;
     private Set<Pet> pets;
 }
