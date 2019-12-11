a7304d69724f79c181c4ee8dc73328fbc18ef76b
ilya <i.dovzhenko26@gmail.com>
2019-12-11 15:05:04 +0700

    Added database entities and all mapping. Closes #37. Closes #38. Closes #39

--- pet-clinic-data/src/main/java/com/springframework/springpetclinic/model/Visit.java
+++ pet-clinic-data/src/main/java/com/springframework/springpetclinic/model/Visit.java
@@ -2,6 +2,8 @@ package com.springframework.springpetclinic.model;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
+
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
@@ -13,6 +15,7 @@ import java.time.LocalDate;
 @Entity
 @Table(name = "visits")
 public class Visit extends BaseEntity {
+    @DateTimeFormat(pattern = "yyyy-MM-DD HH24:mm")
     private LocalDate date;
     private String description;
 
