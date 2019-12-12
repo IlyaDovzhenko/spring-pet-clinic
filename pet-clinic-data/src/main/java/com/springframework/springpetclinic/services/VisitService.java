0a586f1473611d95a34cb68b3c26921217b30ecb
ilya <i.dovzhenko26@gmail.com>
2019-12-12 12:36:59 +0700

    Added visit map service. Closes #47

---
+++ pet-clinic-data/src/main/java/com/springframework/springpetclinic/services/VisitService.java
@@ -0,0 +1,6 @@
+package com.springframework.springpetclinic.services;
+
+import com.springframework.springpetclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+}
