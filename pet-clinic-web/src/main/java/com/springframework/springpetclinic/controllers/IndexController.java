03e4f89ae7bb57fe84594e2250f53addae7eb7de
ilya <i.dovzhenko26@gmail.com>
2020-01-09 17:30:42 +0700

    Added index controller tests

--- pet-clinic-web/src/main/java/com/springframework/springpetclinic/controllers/IndexController.java
+++ pet-clinic-web/src/main/java/com/springframework/springpetclinic/controllers/IndexController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class IndexController {
 
-    @GetMapping({"", "/", "index", "index.html"})
+    @GetMapping({"", "/", "/index", "/index.html"})
     public String index() {
         return "index";
     }
---
+++ pet-clinic-web/src/test/java/com/springframework/springpetclinic/controllers/IndexControllerTest.java
@@ -0,0 +1,42 @@
+package com.springframework.springpetclinic.controllers;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+@ExtendWith(MockitoExtension.class)
+class IndexControllerTest {
+
+    @InjectMocks
+    private IndexController indexController;
+
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    void setUp() {
+        mockMvc = MockMvcBuilders.standaloneSetup(indexController).build();
+    }
+
+    @Test
+    void index() throws Exception {
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("index"));
+    }
+
+    @Test
+    void indexByIndex() throws Exception {
+        mockMvc.perform(get("/index.html"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("index"));
+    }
+}
