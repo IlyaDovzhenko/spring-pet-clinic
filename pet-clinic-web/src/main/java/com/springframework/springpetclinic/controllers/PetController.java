92bb75c8381b3d9f51a1f05c505f23a330087f03
ilya <i.dovzhenko26@gmail.com>
2020-03-16 21:50:49 +0700

    Added model attributes for pets. #56

--- pet-clinic-web/src/main/java/com/springframework/springpetclinic/controllers/PetController.java
+++ pet-clinic-web/src/main/java/com/springframework/springpetclinic/controllers/PetController.java
@@ -1,27 +1,53 @@
 package com.springframework.springpetclinic.controllers;
 
+import com.springframework.springpetclinic.model.Owner;
+import com.springframework.springpetclinic.model.PetType;
+import com.springframework.springpetclinic.services.OwnerService;
 import com.springframework.springpetclinic.services.PetService;
+import com.springframework.springpetclinic.services.PetTypeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collection;
 
 @Controller
+@RequestMapping("owners/{ownerId}")
 public class PetController {
 
+    private static final String CREATE_OR_UPDATE_PET_FORM_VIEW = "pets/createOrUpdatePetForm";
+
     private final PetService petService;
+    private final PetTypeService petTypeService;
+    private final OwnerService ownerService;
 
-    public PetController(PetService petService) {
+    public PetController(PetService petService,
+                         OwnerService ownerService,
+                         PetTypeService petTypeService) {
         this.petService = petService;
+        this.ownerService = ownerService;
+        this.petTypeService = petTypeService;
     }
 
-    @GetMapping({"/pets","pets.html"})
-    public String petsList(Model model) {
-        model.addAttribute("pets", petService.findAll());
-        return "pets/pets_list";
+//    @GetMapping({"/pets","pets.html"})
+//    public String petsList(Model model) {
+//        model.addAttribute("pets", petService.findAll());
+//        return "pets/pets_list";
+//    }
+
+    @ModelAttribute("types")
+    public Collection<PetType> populatePetType() {
+        return petTypeService.findAll();
     }
 
-    @GetMapping("/pets/find")
-    public String findPet() {
-        return "notImplemented";
+    @ModelAttribute("owners")
+    public Owner findOwner(@PathVariable Long ownerId) {
+        return ownerService.findById(ownerId);
+    }
+
+    @InitBinder("owner")
+    public void initOwnerBinder(WebDataBinder dataBinder) {
+        dataBinder.setDisallowedFields("id");
     }
 }
